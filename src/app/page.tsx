aaab8c2338168ea483cc9ce62463f4b03c3c303b
vromariz <viniciusrfscabral@gmail.com>
2025-04-07 15:20:38 -0300

    rodando com ajuda do lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,39 @@
-
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
-            Inicio do duelo
+            Início do duelo
           </li>
         </ol>
+        <div>
+          oi
+        </div>
 
+        <table className="border border-gray-300 text-sm text-center font-[family-name:var(--font-geist-mono)]">
+          <thead className="bg-blck-100">
+            <tr>
+              <th className="px-4 py-2 border border-gray-300">Participante</th>
+              <th className="px-4 py-2 border border-gray-300">Pontos</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="px-4 py-2 border border-gray-300">Carol</td>
+              <td className="px-4 py-2 border border-gray-300">0</td>
+            </tr>
+            <tr>
+              <td className="px-4 py-2 border border-gray-300">Pedro</td>
+              <td className="px-4 py-2 border border-gray-300">0</td>
+            </tr>
+            <tr>
+              <td className="px-4 py-2 border border-gray-300">Vinicius</td>
+              <td className="px-4 py-2 border border-gray-300">0</td>
+            </tr>
+          </tbody>
+        </table>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Carol
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Pedro
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Vinicius
-        </a>
-      </footer>
     </div>
   );
 }
